df68419374fb7daa0fc00b493f6a7a68ad1cdd06
Anders <o.aleksandrov@gmail.com>
2015-09-21 23:39:34 +0300

    ++

---
+++ chess/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess
+{
+    public static class Extensions
+    {
+        public static IEnumerable<Tuple<T, T>> AllCombinations<T>(this IEnumerable<T> collection)
+        { 
+            return collection.SelectMany(e1=>collection.Select(e2=>Tuple.Create(e1,e2)));
+        }
+    }
+}
--- chess/ViewModels/MainViewModel.cs
+++ chess/ViewModels/MainViewModel.cs
@@ -9,7 +9,7 @@ using UIFoundation;
 namespace chess.ViewModels
 {
     public enum States
-    { 
+    {
         State1,
         State2
     }
@@ -39,7 +39,7 @@ namespace chess.ViewModels
         }
 
         public string PiesType { get; set; }
-        
+
     }
 
     public class MainViewModel : BindableBase
@@ -51,12 +51,14 @@ namespace chess.ViewModels
             set { this.SetProperty(ref this.chessPieces, value); }
         }
 
-        public IEnumerable<Tuple<int,int>> possibleMoves;
+        public IEnumerable<Tuple<int, int>> possibleMoves;
         public IEnumerable<Tuple<int, int>> PossibleMoves
         {
             get { return this.possibleMoves; }
             set { this.SetProperty(ref this.possibleMoves, value); }
         }
+
+        private ChessPiece activePiece = null;
 
         public ICommand MoveStep1Command { get; private set; }
         public ICommand MoveStep2Command { get; private set; }
@@ -68,28 +70,39 @@ namespace chess.ViewModels
             get { return state; }
             set { this.SetProperty(ref this.state, value); }
         }
-        
+
 
         public MainViewModel()
         {
             this.State = States.State1;
             this.MoveStep1Command = new Command(MoveStep1);
             this.MoveStep2Command = new Command(MoveStep2);
-            this.ChessPieces = new List<ChessPiece>() { new ChessPiece(2, 2), new ChessPiece(2, 2), new ChessPiece(3, 3) };
+            this.ChessPieces = new List<ChessPiece>() { new ChessPiece(1, 1), new ChessPiece(2, 2), new ChessPiece(3, 3) };
         }
 
         private void MoveStep1(object p)
         {
             var val = p as ChessPiece;
             if (val != null)
-            { 
-                //possibleMoves = new []{-1,1}.GroupBy((i)=>i).Select()
+            {
+                this.PossibleMoves = new[] { -1, 1 }.AllCombinations()
+                    .Select(t => Tuple.Create(val.X - t.Item1, val.Y - t.Item2))
+                    .Where(ft => ft.Item1 >= 0 && ft.Item1 < Constants.BoardSize && ft.Item2 >= 0 && ft.Item2 < Constants.BoardSize).ToArray();
+                this.activePiece = val;
+                this.State = States.State2;
             }
+            
         }
 
         private void MoveStep2(object p)
         {
-
+            var val = p as Tuple<int, int>;
+            if (val != null && this.activePiece != null)
+            {
+                this.activePiece.X = val.Item1;
+                this.activePiece.Y = val.Item2;
+                this.State = States.State1;
+            }
         }
     }
 }
